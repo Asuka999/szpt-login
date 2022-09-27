df33ef170e17a64360d40667930b6352e6079892
asuka999_ <admin@admin>
2022-09-27 09:03:02 +0800

    update: add fun GetInstance()

--- login/login.go
+++ login/login.go
@@ -14,45 +14,46 @@ type loginReply interface {
 	GetCookiesMap() (cookies []*http.Cookie)
 	GetClinet() *http.Client
 	GetJar() *cookiejar.Jar
+	GetInstance() *User
 }
 
-func (U *user) GetCookiesString() string {
+func (U *User) GetCookiesString() string {
 	Json, _ := U.Jar.MarshalJSON()
 
 	return string(Json)
 }
 
-func (U *user) GetCookiesMap() (cookies []*http.Cookie) {
+func (U *User) GetCookiesMap() (cookies []*http.Cookie) {
 	cookies = U.Jar.AllCookies()
 	return cookies
 }
 
-func (U *user) GetClinet() *http.Client {
-	return U.clinet
+func (U *User) GetClinet() *http.Client {
+	return U.Clinet
 }
 
 func Login(Account string, Passwrod string) (loginReply, error) {
-	user := &user{
-		account:  Account,
-		passwrod: Passwrod,
+	User := &User{
+		Account:  Account,
+		Password: Passwrod,
 	}
-	user.newHttpClinet()
-	user.getEncryInfo()
-	err := user.login()
+	User.newHttpClinet()
+	User.getEncryInfo()
+	err := User.login()
 
 	if err != nil {
 		return nil, err
 	}
-	return user, nil
+	return User, nil
 }
 
-type user struct {
-	account        string
-	passwrod       string
+type User struct {
+	Account        string
+	Password       string
 	lt             string
 	pwdEncryptSalt string
 	Jar            *cookiejar.Jar
-	clinet         *http.Client
+	Clinet         *http.Client
 	encryptedPwd   string
 }
 
@@ -61,14 +62,14 @@ func setProxyUrl() (proxyUrl *url.URL) {
 	return proxyUrl
 }
 
-func (U *user) GetJar() *cookiejar.Jar {
+func (U *User) GetJar() *cookiejar.Jar {
 	return U.Jar
 }
 
-func (U *user) newHttpClinet() {
+func (U *User) newHttpClinet() {
 	U.Jar, _ = cookiejar.New(nil)
 
-	U.clinet = &http.Client{
+	U.Clinet = &http.Client{
 		Transport: &http.Transport{
 			// set proxyman proxy
 			//Proxy: http.ProxyURL(setProxyUrl()),
@@ -76,24 +77,27 @@ func (U *user) newHttpClinet() {
 		Jar: U.Jar,
 	}
 }
+func (U *User) GetInstance() *User {
+	return U
+}
 
-func (U *user) getEncryInfo() {
-	resp, _ := U.clinet.Get("https://authserver.szpt.edu.cn/authserver/login")
+func (U *User) getEncryInfo() {
+	resp, _ := U.Clinet.Get("https://authserver.szpt.edu.cn/authserver/login")
 	U.lt, U.pwdEncryptSalt = utils.GetEncry(resp)
-	U.encryptedPwd = utils.EncryPasswd(U.passwrod, U.pwdEncryptSalt)
+	U.encryptedPwd = utils.EncryPasswd(U.Password, U.pwdEncryptSalt)
 
 }
 
-func (U *user) login() error {
-	requestForm := strings.NewReader(url.Values{"username": {U.account}, "password": {U.encryptedPwd}, "lt": {U.lt}, "dllt": {"userNamePasswordLogin"}, "execution": {"e1s1"}, "_eventId": {"submit"}, "rmShown": {"1"}}.Encode())
+func (U *User) login() error {
+	requestForm := strings.NewReader(url.Values{"Username": {U.Account}, "password": {U.encryptedPwd}, "lt": {U.lt}, "dllt": {"UserNamePasswordLogin"}, "execution": {"e1s1"}, "_eventId": {"submit"}, "rmShown": {"1"}}.Encode())
 	req, _ := http.NewRequest("POST", "https://authserver.szpt.edu.cn/authserver/login", requestForm)
 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-	loginResp, _ := U.clinet.Do(req)
+	loginResp, _ := U.Clinet.Do(req)
 	if loginResp.Request.URL.Path == "/authserver/index.do" {
-		U.clinet.Get("https://ehall.szpt.edu.cn/publicappinternet/sys/szptpubxsjkxxbs/*default/index.do#/")
+		U.Clinet.Get("https://ehall.szpt.edu.cn/publicappinternet/sys/szptpubxsjkxxbs/*default/index.do#/")
 		menuinfoForm := "data=%7B%22APPID%22%3A%225812981499622390%22%2C%22APPNAME%22%3A%22szptpubxsjkxxbs%22%7D"
 		menuinfoForms := strings.NewReader(menuinfoForm)
-		U.clinet.Post("https://ehall.szpt.edu.cn/publicappinternet/sys/itpub/MobileCommon/getMenuInfo.do", "application/x-www-form-urlencoded", menuinfoForms)
+		U.Clinet.Post("https://ehall.szpt.edu.cn/publicappinternet/sys/itpub/MobileCommon/getMenuInfo.do", "application/x-www-form-urlencoded", menuinfoForms)
 		return nil
 	}
 	return errors.New("提供的用户名或者密码有误")
